50fbb667cdb70f82cbafe125b4cc682de6d2f462
Ellis <147318799+NietzschePoseidon@users.noreply.github.com>
2026-03-04 14:26:35 +0300

    Update task_2-5_nucl_count.py

--- projects_2/task_2_5/task_2-5_nucl_count.py
+++ projects_2/task_2_5/task_2-5_nucl_count.py
@@ -11,10 +11,15 @@ def nucleotide_count():
     g_count = sequence.count("G")
     c_count = sequence.count("C")
     len_sequence = len(sequence)
+    a_procent = a_count / len_sequence
+    t_procent = t_count / len_sequence
+    g_procent = g_count / len_sequence
+    c_procent = c_count / len_sequence
 
-    return a_count, t_count, g_count, c_count, len_sequence
+    return a_count, t_count, g_count, c_count, len_sequence, a_procent, t_procent, g_procent, c_procent
 
 result = nucleotide_count()
 
 print(f"\nПодсчёт нуклеотидов:\nA:\t{result[0]}\nT:\t{result[1]}\nG:\t{result[2]}\nC:\t{result[3]}")
 print(f"\nОбщая длина: {result[4]}")
+print(f"\nПроцентное содержание нуклеотидов:\nA:\t{result[5]:.2f}\nT:\t{result[6]:.2f}\nG:\t{result[7]:.2f}\nC:\t{result[8]:.2f}")
